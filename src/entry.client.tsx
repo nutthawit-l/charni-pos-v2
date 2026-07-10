5ae0ab97fb85c5d2e196957b2b138fb31fdd9168
Nutthawit Lilawannasin <nutthawit.ll@proton.me>
2026-07-11 00:08:47 +0700

    feat(pwa): add registerSW() to src/entry.client.tsx

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
+import { registerSW } from 'virtual:pwa-register';
 import './index.css';
+
+registerSW({ immediate: true });
 
 hydrateRoot(document, 
     <StrictMode>
